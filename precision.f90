ff5671fb4869c53c7b2032cc794e021332217f6f
EdoCascio <123598523+EdoCascio@users.noreply.github.com>
2023-01-25 22:55:32 +0100

    Create precision.f90

---
+++ precision.f90
@@ -0,0 +1,6 @@
+module precision
+  ! Double precision
+  integer, parameter, public :: dp = selected_real_kind(14) 
+  ! Quadrupole precision
+  ! integer, parameter, public :: dp = selected_real_kind(30) 
+end module precision
